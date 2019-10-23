e06c2c99732fc5c4e14a41eef371a7861674b4d5
Marouane ER-Rouhi <marouane.erouhi@gmail.com>
2019-10-23 15:20:22 -0400

    Added room switching

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    [SerializeField] GameObject[] RoomLocations;
+    [SerializeField] int CurrentRoom;
+    [SerializeField] GameObject cam;
+    void Start() {
+        
+    }
+
+    void Update() {
+        cam.transform.position = RoomLocations[CurrentRoom].gameObject.transform.position;
+    }
+}
