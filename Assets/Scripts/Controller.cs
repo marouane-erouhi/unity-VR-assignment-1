948d5d620c0a259883e924ae84660166b7987cb0
Marouane ER-Rouhi <marouane.erouhi@gmail.com>
2019-11-06 16:25:16 -0500

    Added Oculus go controller with proper rotation

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    void Update() {
+        Vector3 r = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote).eulerAngles;
+        Vector3 e = new Vector3(r.x,r.y+90,r.z);
+        transform.rotation = Quaternion.Euler(e);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
     [SerializeField] GameObject[] RoomLocations;
+    int maxRooms = 2;
     [SerializeField] int CurrentRoom;
     [SerializeField] GameObject cam;
     void Start() {
@@ -12,5 +13,20 @@ public class GameManager : MonoBehaviour {
 
     void Update() {
         cam.transform.position = RoomLocations[CurrentRoom].gameObject.transform.position;
+    }
+
+    public void SetCurrentRoom(int val){
+        if(val > maxRooms)  CurrentRoom = maxRooms;
+        else if(val < 0)    CurrentRoom = 0;
+        else                CurrentRoom = val;
+    }
+    public int GetCurrentRoom(){
+        return CurrentRoom;
+    }
+    public void NextRoom(){
+        CurrentRoom = CurrentRoom==maxRooms ? 0 : ++CurrentRoom; //FLAG: potential bug, test later
+    }
+    public void PreviousRoom(){
+        CurrentRoom = CurrentRoom==0 ? maxRooms : --CurrentRoom; //FLAG: potential bug, test later
     }
 }
