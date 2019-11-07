d4bc09860083a9556b5634d23b4475a87d12bc4c
Marouane ER-Rouhi <marouane.erouhi@gmail.com>
2019-11-07 15:48:55 -0500

    Added line renderer to controller

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
     [SerializeField] GameObject LaserStart;
+    LineRenderer lineRenderer;
+    LineDrawer lineDrawer;
     void Start() {
-
+        lineRenderer = GetComponent<LineRenderer>();
+        lineDrawer = new LineDrawer(lineRenderer);
     }
 
     void Update() {
@@ -25,8 +28,76 @@ public class Controller : MonoBehaviour {
             //show map
         }
 
+        //line renderer
+        lineDrawer.DrawLineInGameView(LaserStart.transform.position, 
+            LaserStart.transform.position+LaserStart.transform.forward*50, 
+            Color.blue);
 
 
 
+    }
+}
+
+
+
+
+public struct LineDrawer
+{
+    LineRenderer lineRenderer;
+    private float lineSize;
+
+    public LineDrawer(LineRenderer lineRenderer_, float lineSize = 0.05f)
+    {
+        GameObject lineObj = new GameObject("LineObj");
+        lineRenderer = lineRenderer_;
+        //Particles/Additive
+        lineRenderer.material = new Material(Shader.Find("Default-Line"));
+        
+        this.lineSize = lineSize;
+    }
+
+    private void init(float lineSize = 0.05f)
+    {
+        if (lineRenderer == null)
+        {
+            GameObject lineObj = new GameObject("LineObj");
+            lineRenderer = lineObj.AddComponent<LineRenderer>();
+            //Particles/Additive
+            lineRenderer.material = new Material(Shader.Find("Hidden/Internal-Colored"));
+
+            this.lineSize = lineSize;
+        }
+    }
+
+    //Draws lines through the provided vertices
+    public void DrawLineInGameView(Vector3 start, Vector3 end, Color color)
+    {
+        if (lineRenderer == null)
+        {
+            init(0.05f);
+        }
+
+        //Set color
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+
+        //Set width
+        lineRenderer.startWidth = lineSize;
+        lineRenderer.endWidth = lineSize;
+
+        //Set line count which is 2
+        lineRenderer.positionCount = 2;
+
+        //Set the postion of both two lines
+        lineRenderer.SetPosition(0, start);
+        lineRenderer.SetPosition(1, end);
+    }
+
+    public void Destroy()
+    {
+        if (lineRenderer != null)
+        {
+            UnityEngine.Object.Destroy(lineRenderer.gameObject);
+        }
     }
 }
